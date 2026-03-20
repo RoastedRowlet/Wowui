87a1231c45bc57745dcba1bbcb36efbd5ae72d44
RoastedRowlet <broderick.chris94@gmail.com>
2026-03-20 16:35:31 -0400

    Automated commit on Fri 03/20/2026 16:35:31.07

--- backup.bat
+++ backup.bat
@@ -1,5 +1,12 @@
 @echo off
-cd /d D:\World of Warcraft\_retail_
+rmdir /S /Q Cache
+rmdir /S /Q Interface
+rmdir /S /Q WTF
+
+xcopy "D:\World of Warcraft\_retail_\Cache" "D:\WoWUI\Wowui\Cache" /s /e /i /h
+xcopy "D:\World of Warcraft\_retail_\Interface" "D:\WoWUI\Wowui\Interface" /s /e /i /h
+xcopy "D:\World of Warcraft\_retail_\WTF" "D:\WoWUI\Wowui\WTF" /s /e /i /h
+
 :: Stage all changes (new, modified, and deleted files)
 git add -A
 
